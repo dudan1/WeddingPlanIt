8a105899c721c3e089254b4ec82a92ff529cc4f7
1809591 <1809591@rgu.ac.uk>
2019-02-18 15:12:38 +0000

    php

--- PHP_database_insert/test-db.php
+++ PHP_database_insert/test-db.php
@@ -7,3 +7,15 @@ $dbname = "db1809591_cmm007";
 // Create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($connection) . PHP_EOL;
+
+mysqli_close($connection);
+?>;
