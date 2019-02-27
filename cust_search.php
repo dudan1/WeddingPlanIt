633f909138c97e76be8fb5b17c4f6b05193ca4b8
Joanne2019 <1800903@rgu.ac.uk>
2019-02-27 13:54:54 +0000

    customer search page incomplete

---
+++ cust_search.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Customer Search Page</title>
+    <link rel="stylesheet" type="text/css" href="CSS/unsemantic-grid-responsive-tablet.css">
+    <style>
+        .row{
+            max-width: 1200px;
+            margin: auto;
+        }
+        .logo img{
+            width: 200px;
+            height:auto;
+            float: left;
+        }
+        .main-nav{
+            float: right;
+            list-style: none;
+            margin-top: 30px;
+        }
+        .main-nav li{
+            display:inline-block;
+        }
+        .main-nav li a{
+            color: darkseagreen;
+            text-decoration: none;
+            padding: 5px 35px;
+            font-family: "Trebuchet MS","Helvetica", "Sans-serif";
+            font-size: 20px;
+        }
+        .main-nav li a:hover{
+            border: 1px goldenrod;
+        }
+   body{
+       height: 100vh;
+       background-size: cover;
+       background-position: center;
+   }
+
+
+    </style>
+</head>
+
+<body style=" background-image:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(assets/images/wed.jpg);">
+
+<header>
+    <div class="row">
+    <div class="logo">
+<img src="assets/images/logo1.png" alt="wedding band">
+    </div>
+    <nav>
+        <ul class="main-nav">
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">CONTACT US</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">LOG OUT</a></li>
+            <li><a href="#">MY PLAN</a></li>
+            <li><a href="#">MY PROFILE</a></li>
+        </ul>
+    </nav>
+    </div>
+</header>
+
+<main>
+
+
+</main>
+
+
+<?php
+
+?>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
