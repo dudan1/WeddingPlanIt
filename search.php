e5a7b23aeb92a71d47d4ff6d7daf630fe903f89e
tiwobanda <t.banda@rgu.ac.uk>
2019-03-02 12:49:58 +0000

    Service provider search working on user landing page. Session inserted, but commented out for now

--- search.php
+++ search.php
@@ -6,7 +6,7 @@
     <title>Service Provider Search Page</title>
     <link rel="stylesheet" type="text/css" href="CSS/styles.css">
     <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
-    <!--<link rel="stylesheet" type="text/css" href="CSS/unsemantic-grid-responsive-tablet.css">-->
+    <link rel="stylesheet" type="text/css" href="CSS/unsemantic-grid-responsive-tablet.css">-->
 
     <link rel="shortcut icon" href="assets/favicons/favicon.ico" type="image/x-icon">
     <link rel="icon" href="assets/favicons/favicon.ico" type="image/x-icon">
--- search_providers.php
+++ search_providers.php
@@ -10,7 +10,7 @@ session_start();
     <title>Customer Homepage</title>
     <link rel="stylesheet" type="text/css" href="CSS/styles.css">
     <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
-    <link rel="stylesheet" type="text/css" href="CSS/unsemantic-grid-responsive-tablet.css">-->
+    <!--<link rel="stylesheet" type="text/css" href="CSS/unsemantic-grid-responsive-tablet.css">-->
 
     <link rel="shortcut icon" href="assets/favicons/favicon.ico" type="image/x-icon">
     <link rel="icon" href="assets/favicons/favicon.ico" type="image/x-icon">
@@ -43,11 +43,11 @@ session_start();
         .main-nav li a:hover{
             border: 1px goldenrod;
         }
-         body{
+        body{
             height: 100vh;
             background-size: cover;
             background-position: center;
-   }
+        }
     </style>
 </head>
 
@@ -55,18 +55,18 @@ session_start();
 
 <header>
     <div class="row">
-    <div class="logo">
-<img src="assets/images/logo1.png" alt="wedding band">
-    </div>
-    <nav>
-        <ul class="main-nav">
-            <li><a href="cust_home.php">HOME</a></li>
-            <li><a href="#">CONTACT US</a></li>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">MY PLAN</a></li>
-            <li><a href="PHP_database_insert/logout.php">LOG OUT</a></li>
-        </ul>
-    </nav>
+        <div class="logo">
+            <img src="assets/images/logo1.png" alt="wedding band">
+        </div>
+        <nav>
+            <ul class="main-nav">
+                <li><a href="cust_home.php">HOME</a></li>
+                <li><a href="#">CONTACT US</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">MY PLAN</a></li>
+                <li><a href="PHP_database_insert/logout.php">LOG OUT</a></li>
+            </ul>
+        </nav>
     </div>
     <p><button class="btn3 info3" onclick="document.getElementById('id02').style.display='block'" style="width:110px;height:auto;float:right">Profile</button></p>
     <div id="id02" class="modal">
@@ -121,6 +121,43 @@ session_start();
 
 
     </div>
+
+    <div class="row">
+        <div class="col-sm-2"></div>
+
+
+        <div class="col-sm-8">
+
+            <?php
+            //check connection
+            require_once('PHP_database_insert/db.php');
+            if ($connection === false) {
+                die ("Error: could not connect. " . mysqli_connect_error());
+            }
+            //assign search variable
+            $search_value = $_GET['search_value'];
+            //run query
+            $sql = "SELECT * from service_provider WHERE category = '$search_value'";
+            if ($result = mysqli_query($connection, $sql)) {
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_array($result)) {
+                        //while($row = $result->fetch_array()){
+                        echo "<div class='card'>";
+                        echo "<div class='card-header'>" . $row['business_name'] . "</div>";
+                        echo "<div class='card-body'>" . $row['category'] . "</div>";
+                        echo "</div>" . "<br>";
+                    }
+//free result set
+                    mysqli_free_result($result);
+                }else{
+                    echo "No records matching your query were found.";
+                }
+            }
+            ?>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
+
 </main>
 
 
