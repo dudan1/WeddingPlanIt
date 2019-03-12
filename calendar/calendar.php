f83acb2beb337cbc5180cb03b7ce46789e43584c
1809591 <1809591@rgu.ac.uk>
2019-03-12 20:59:34 +0000

    Calendar Working

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -14,7 +14,6 @@ $count =mysqli_num_rows($result);
 if($count == 1) {
     $business_name = $row['business_name'];
 }
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
