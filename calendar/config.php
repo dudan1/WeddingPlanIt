df5def8537dd39737a48c7789500914b708c9d4c
0006664 <0006664@rgu.ac.uk>
2019-03-12 10:53:38 +0000

    NCF 20190227 Changed Cust_Home.php and SP_Home.php

---
+++ calendar/config.php
@@ -0,0 +1,11 @@
+<?php
+	$servername = "localhost";
+	$username = "user";
+	$password = "pass";
+	$dbname = "database";
+	$tablename = "bookingcalendar";
+
+	// translate these
+	$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+	$headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+?>
--- test-insert/test-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "CSDM-WEBDEV";
-$username = "1809591";
-$password = "1809591";
-$dbname = "db1809591_cmm007";
-
-// Create connection
-$connection = mysqli_connect($servername, $username, $password, $dbname);
-
