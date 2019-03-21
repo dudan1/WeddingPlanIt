434a13f5efd7153747f65537dbe69f2ac03d7a78
0006664 <0006664@rgu.ac.uk>
2019-03-21 16:51:20 +0000

    Upload basic carousel

--- contact_us.php
+++ contact_us.php
@@ -64,14 +64,7 @@
 </main>
 
 <footer class="footer">
-    <!--<p>DJNT (c) 2019</p>
-    <p>Join us on</p>
-    <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-twitter"></a>
-    <a href="#" class="fa fa-google"></a>
-    <a href="#" class="fa fa-instagram"></a>
-    <a href="#" class="fa fa-youtube"></a>
-    -->
+
 
 </footer>
 
