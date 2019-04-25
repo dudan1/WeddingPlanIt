922c9016d045b912d754971f0e24beb6425e8d96
1809591 <1809591@rgu.ac.uk>
2019-04-25 11:44:11 +0100

    fixed navbars

--- Templates/navbar/navbar_sp_cal.php
+++ Templates/navbar/navbar_sp_cal.php
@@ -3,16 +3,16 @@
 
 <div class="topnav" id="myTopnav">
     <div>
-        <a href="index.php"><img src="assets/images/logo1.png" alt="image link back to portal"
+        <a href="../index.php"><img src="assets/images/logo1.png" alt="image link back to portal"
                                  title="image link back to portal" class="responsive"></a>
     </div>
-    <a href="cust_home.php">Homepage</a>
-    <a href="faq_sp.php">About Us/FAQs</a>
-    <a href="contact_us_sp.php">Contact Us</a>
-    <a href="my_calendar.php">Calendar</a>
-    <a href="sp_profile.php">Profile</a>
-    <a href="pic_upload.php">Manage My Photos</a>
-    <a href="PHP_database_insert/logout.php">Logout</a>
+    <a href="../sp_home.php">Homepage</a>
+    <a href="../faq_sp.php">About Us/FAQs</a>
+    <a href="../contact_us_sp.php">Contact Us</a>
+    <a href="../my_calendar.php">Calendar</a>
+    <a href="../sp_profile.php">Profile</a>
+    <a href="../pic_upload.php">Manage My Photos</a>
+    <a href="../PHP_database_insert/logout.php">Logout</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
     </a>
