c10769d74dcbfe16f4789a8f4bc0515e636ea6a7
0006664 <0006664@rgu.ac.uk>
2019-04-25 12:25:15 +0100

    Fixed SP Calendar NavBar issue

--- Templates/navbar/navbar_sp_cal.php
+++ Templates/navbar/navbar_sp_cal.php
@@ -6,13 +6,13 @@
         <a href="../index.php"><img src="assets/images/logo1.png" alt="image link back to portal"
                                  title="image link back to portal" class="responsive"></a>
     </div>
-    <a href="../sp_home.php">Homepage</a>
-    <a href="../faq_sp.php">About Us/FAQs</a>
-    <a href="../contact_us_sp.php">Contact Us</a>
-    <a href="../my_calendar.php">Calendar</a>
-    <a href="../sp_profile.php">Profile</a>
-    <a href="../pic_upload.php">Manage My Photos</a>
-    <a href="../PHP_database_insert/logout.php">Logout</a>
+    <a href="sp_home.php">Homepage</a>
+    <a href="faq_sp.php">About Us/FAQs</a>
+    <a href="contact_us_sp.php">Contact Us</a>
+    <a href="my_calendar.php">Calendar</a>
+    <a href="sp_profile.php">Profile</a>
+    <a href="pic_upload.php">Manage My Photos</a>
+    <a href="PHP_database_insert/logout.php">Logout</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
     </a>
